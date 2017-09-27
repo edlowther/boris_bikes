855f3ca53e3b06775d91d0270ebed485b466f3d2
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-27 11:49:31 +0100

    Added tests for working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to 'release_bike' }
+  it 'returns a working bike when asked to release_bike' do
+    expect(subject.release_bike.working?).to eq true
+  end
 end
